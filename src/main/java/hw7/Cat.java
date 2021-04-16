e241db54fd52269619ac527237909c08dd405a63
Kseniia Grafskaia <xgrafskaya@gmail.com>
2021-04-16 12:11:13 +0300

    Homework 7

---
+++ src/main/java/hw7/Cat.java
@@ -0,0 +1,35 @@
+package hw7;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+    private boolean fullness;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.fullness = false;
+    }
+
+    public void eat(Plate p) {
+        if (appetite <= p.getFood()) {
+            p.decreaseFood(appetite);
+            fullness = true;
+        }
+        else {
+            System.out.println("There is no enough food in this plate for " + name);
+        }
+
+    }
+
+    public boolean isFullness() {
+        return fullness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
